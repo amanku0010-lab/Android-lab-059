00dd72a6bedce6b47dbd1b06e6267a668c7f89e5
amanku0010-lab <aman.ku0010@gmail.com>
2026-05-01 12:06:30 +0530

    Update Data.java

--- Intermediate/DetailApplication/Data.java
+++ Intermediate/DetailApplication/Data.java
@@ -1,1 +1,16 @@
+package com.example.masterdetail;
 
+public class Data {
+
+    public static String[] names = {
+            "Aman", "Rahul", "Sneha", "Karan", "Priya"
+    };
+
+    public static String[] details = {
+            "Aman\nAndroid Developer",
+            "Rahul\nBackend Developer",
+            "Sneha\nDesigner",
+            "Karan\nML Engineer",
+            "Priya\nDevOps"
+    };
+}
