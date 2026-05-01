68b78bba496f1be9628259da2368ff32996a87b6
amanku0010-lab <aman.ku0010@gmail.com>
2026-05-01 12:05:58 +0530

    Update DetailActivity.java

--- Intermediate/DetailApplication/DetailActivity.java
+++ Intermediate/DetailApplication/DetailActivity.java
@@ -1,1 +1,21 @@
+package com.example.masterdetail;
 
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.activity_detail);
+
+        int i = getIntent().getIntExtra("id", 0);
+
+        if (b == null) {
+            DetailFragment f = DetailFragment.newInstance(i);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.detail, f)
+                    .commit();
+        }
+    }
+}
